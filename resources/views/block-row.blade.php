f5b068ea46aad86317269de6f7c7b1ff7796cf91
stichtw <wolfgang.sticht@o2.com>
2018-01-03 18:04:57 +0100

    block-row: option to position label at the top added

--- resources/views/block-row.blade.php
+++ resources/views/block-row.blade.php
@@ -2,12 +2,20 @@
     if (isset($labelTooltip) == false && strlen($label ?? '') > 20) {
         $labelTooltip = $label ?? '';
     }
+    if(isset($labelTop) == false) {
+        $labelTop = false;
+    }
+    if($labelTop) {
+        $labelStyle = 'block-left d-flex align-items-top px-2 py-1';
+    } else {
+        $labelStyle = 'block-left d-flex align-items-center px-2 py-1';
+    }
 @endphp
 <div class="block-row row rounded {{ $class ?? '' }}"
      @isset($id) id="{{ $id }}" @endisset
      @isset($style) style="{{ $style }}" @endisset
 >
-    <div class="block-left d-flex align-items-center px-2 py-1">
+    <div class="{{ $labelStyle }}">
         <div class="text-truncate"
              @isset($labelTooltip) title="{{ $labelTooltip }}" data-toggle="tooltip" data-placement="top" @endisset
         >
