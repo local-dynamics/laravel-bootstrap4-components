a09e7f93805e8223344c9af98657034a06443154
xandor <thomas@ld-n.de>
2018-01-11 10:16:45 +0100

    update alignment

--- resources/views/block-row.blade.php
+++ resources/views/block-row.blade.php
@@ -6,23 +6,23 @@
         $labelTop = false;
     }
     if($labelTop) {
-        $labelStyle = 'block-left d-flex-column align-items-top px-2 py-1';
+        $labelStyle = '';
     } else {
-        $labelStyle = 'block-left d-flex-column align-items-center px-2 py-1';
+        $labelStyle = 'align-items-center';
     }
 @endphp
-<div class="block-row row rounded {{ $class ?? '' }} d-flex "
+<div class="block-row row rounded {{ $class ?? '' }} d-flex"
      @isset($id) id="{{ $id }}" @endisset
      @isset($style) style="{{ $style }}" @endisset
 >
-    <div class="{{ $labelStyle }}">
+    <div class="block-left d-flex px-2 py-1 {{ $labelStyle }}">
         <div class="text-truncate"
              @isset($labelTooltip) title="{{ $labelTooltip }}" data-toggle="tooltip" data-placement="top" @endisset
         >
             {{ $label ?? '' }}
         </div>
     </div>
-    <div class="block-right col d-flex-column align-items-center px-2 py-1">
-        {{ $slot }}
+    <div class="block-right col d-flex align-items-center px-2 py-1">
+        <span>{{ $slot }}</span>
     </div>
 </div>
