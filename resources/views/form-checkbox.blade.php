73a4c61f54be20244eee952159da80fe0e01060a
stichtw <wolfgang.sticht@o2.com>
2018-01-31 14:20:06 +0100

    checkboxes accept custom css classes now

--- resources/views/form-checkbox.blade.php
+++ resources/views/form-checkbox.blade.php
@@ -2,19 +2,24 @@
     $id = $id ?? 'bsComp_chkbx_' . mt_rand(0,10000);
 @endphp
 <div class="custom-control custom-checkbox">
-    <input class="custom-control-input"
-           type="checkbox"
-           id="{{ $id }}"
-           value="{{ empty(trim($slot)) ? '1': trim($slot) }}"
-           @isset($name)
-           name="{{ $name }}"
-           @endisset
-           @if($checked ?? false)
-           checked="checked"
-           @endif
-           @isset($style)
-           style="{{ $style }}"
-        @endisset
+    <input
+            @isset($class)
+            class="custom-control-input {{$class}}"
+            @else
+            class="custom-control-input"
+            @endisset
+            type="checkbox"
+            id="{{ $id }}"
+            value="{{ empty(trim($slot)) ? '1': trim($slot) }}"
+            @isset($name)
+            name="{{ $name }}"
+            @endisset
+            @if($checked ?? false)
+            checked="checked"
+            @endif
+            @isset($style)
+            style="{{ $style }}"
+            @endisset
     >
     <label class="custom-control-label"
            for="{{ $id }}">{{ $label ?? '' }}</label>
