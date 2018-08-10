76a9b569fba403bb5dd4048bdb58c99a80e74a17
Thomas Enßner <thomas@ld-n.de>
2018-08-10 13:20:05 +0200

    use bootstrap-select

--- resources/views/form-select.blade.php
+++ resources/views/form-select.blade.php
@@ -1,15 +1,25 @@
-<select class="form-control custom-select {{ $class ?? '' }}"
+@php($attributes = array_merge(
+    [
+        'data-live-search'       => 'true',
+        'data-select-all-text'   => 'alle auswählen',
+        'data-deselect-all-text' => 'alle abwählen',
+    ],
+    $attributes ?? []
+))
+<select class="form-control custom-select selectpicker {{ $class ?? '' }}"
         @isset($id) id="{{ $id }}" @endisset
         @isset($name) name="{{ $name }}" @endisset
         @isset($style) style="{{ $style }}" @endisset
+        data-live-search="true"
+        data-deselect-all-text="alle abwählen"
+        data-select-all-text="alle auswählen"
+@foreach($attributes ?? [] as $attribute => $value) {{ $attribute }}="{{ $value }}" @endforeach
 >
-    @isset($placeholder)
-        <option value="">{{ $placeholder }}</option>
-    @endisset
+@isset($placeholder)
+    <option value="">{{ $placeholder }}</option>
+@endisset
 
-    @foreach($options as $value => $label)
-        <option value="{{ $value }}"
-                @if($value == (string)$slot) selected @endif
-        >{{ $label }}</option>
-    @endforeach
+@foreach($options as $value => $label)
+    <option value="{{ $value }}" @if($value == (string)$slot) selected @endif>{{ $label }}</option>
+@endforeach
 </select>
